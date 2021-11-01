721b0e0ffe38deae23f800fae1532300acaeb301
ibrahim855 <78034840+ibrahim855@users.noreply.github.com>
2021-11-01 11:18:11 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-
-
 function App() {
-  // const { isLoggedIn} = useContext(AuthContext);
   return (
-     <div>
-        <h1>Another one</h1>
-     </div>
+    <div>
+
+    </div>
   );
 }
 
 export default App;
-
-
-
-// our-damain.com/welcome ==> Welcome Component
-// our-domain.com/products  ==> Product Component
-// our-domani.com/product-details/0.343434
