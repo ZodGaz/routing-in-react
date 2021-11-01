412bc1349413bb93e2df24a3c666bef9b8a1852a
ibrahim855 <78034840+ibrahim855@users.noreply.github.com>
2021-11-01 11:16:00 +0100

    routing in react

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
-// import React, {useContext} from 'react';
-import './App.css';
-import { Route , Switch, Redirect} from 'react-router-dom';
-
-//AuthContext
-// import AuthContext from './context/auth-context';
-
-//Components
-// import Login from './components/Login/Login';
-// import Home from './components/Home/Home';
-
-
-//ROUTING COMPONENTS
-import Welcome from './pages/Welcome';
-import Product from './pages/Product';
-import ProductDetails from './pages/ProductDetails';
-
-
-
-import MainHeader from './otherComponents/MainHeader';
-
 
 
 function App() {
   // const { isLoggedIn} = useContext(AuthContext);
   return (
      <div>
-       <MainHeader />
-       <main>
-       <Switch>
-       <Route path="/" exact>
-          <Redirect to="/welcome/"/>
-       </Route>
-
-       <Route path="/welcome/">
-            <Welcome />
-        </Route>
-
-        <Route path="/products" exact>
-          <Product />
-        </Route>
-        
-        <Route path="/products/:productId">
-          <ProductDetails />
-        </Route>
-
-       
-       </Switch>
-       </main>
-        
+        <h1>Another one</h1>
      </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,23 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { AuthContextProvider } from './context/auth-context';
-import { BrowserRouter } from 'react-router-dom';
 
-// ReactDOM.render(
-//   <AuthContextProvider>
-//     <App />
-//   </AuthContextProvider>,
-//   document.getElementById('root')
-// );
-
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-
